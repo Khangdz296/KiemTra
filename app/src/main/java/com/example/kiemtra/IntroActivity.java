aec09549851a7fe02be23b3d41b1d0ff5bdd4758
minhau45 <minhau11022005@gmail.com>
2025-11-28 15:49:04 +0700

    Update IntroActivity.java

--- app/src/main/java/com/example/kiemtra/IntroActivity.java
+++ app/src/main/java/com/example/kiemtra/IntroActivity.java
@@ -35,6 +35,9 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         if (isLoggedIn()) {
             startActivity(new Intent(this, MainActivity.class));
             finish();
